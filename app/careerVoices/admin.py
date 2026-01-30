30ad3d2d991cb3c1a7b9289ffe2489c83b6fe6be
e1q23079 <e1q23079@oit.ac.jp>
2026-01-31 01:12:13 +0900

    careerVoicesアプリケーションの初期設定と基本ファイルを追加

---
+++ app/careerVoices/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+admin.site.site_header = "Career Voices 管理画面"
+admin.site.site_title = "Career Voices 管理画面"
+admin.site.index_title = "ようこそ Career Voices 管理画面へ"
